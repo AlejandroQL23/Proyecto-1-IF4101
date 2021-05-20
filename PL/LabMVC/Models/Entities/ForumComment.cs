95067f4bd0c4f975ff575e196169daecdf2d39a2
DiegoGarita <diegogarita2413@gmail.com>
2021-05-19 18:55:51 -0600

    Diego: Agrego objeto, DAO y Controller para foro

---
+++ PL/LabMVC/Models/Entities/ForumComment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace LabMVC.Models.Entities
+{
+    public partial class ForumComment
+    {
+        public int Id { get; set; }
+        public string AuthorIdCard { get; set; }
+        public string Author { get; set; }
+        public string TextContent { get; set; }
+        public DateTime? CreationDate { get; set; }
+
+        public virtual User AuthorIdCardNavigation { get; set; }
+    }
+}
